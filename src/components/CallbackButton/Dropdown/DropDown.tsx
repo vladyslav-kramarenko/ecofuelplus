0af2d00a00453dce47650964bdb21fce24404bdb
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-04-01 00:51:54 -0700

    data edits

--- src/components/CallbackButton/Dropdown/DropDown.tsx
+++ src/components/CallbackButton/Dropdown/DropDown.tsx
@@ -5,24 +5,24 @@ const Dropdown: React.FC = () => {
     return (
         <div className="cb-dropdown">
             <a className="select-item" target="_blank" rel="nofollow noreferrer"
-               href="https://t.me/Desire_Antalya_bot">
+               href="https://t.me/EcoFuelplus">
                 <i className="telegram-icon"><span> </span></i>
                 Telegram
             </a>
             <a className="select-item" target="_blank"
                rel="nofollow noreferrer"
-               href="viber://pa?chatURI=desire-antalya">
+               href="viber://contact?number=+380683688623">
                 <i className="viber-icon"><span></span></i>
                 Viber
             </a>
-            <a className="select-item" target="_blank" rel="nofollow noreferrer"
-               href="https://api.whatsapp.com/send/?phone=%2B905373110846&amp;text&amp;type=phone_number&amp;app_absent=0">
-                <i className="whatsapp-icon"><span></span></i>
-                WhatsApp
-            </a>
+            {/*<a className="select-item" target="_blank" rel="nofollow noreferrer"*/}
+            {/*   href="https://api.whatsapp.com/send/?phone=%2B905373110846&amp;text&amp;type=phone_number&amp;app_absent=0">*/}
+            {/*    <i className="whatsapp-icon"><span></span></i>*/}
+            {/*    WhatsApp*/}
+            {/*</a>*/}
             <a className="select-item" href="tel:+1234567890">
                 <i className="phone-icon-black"><span></span></i>
-                +380683688623
+                +38 068 368 8623
                 {/*Call Us*/}
             </a>
         </div>
