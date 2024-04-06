4f3dbf13470b38c96f236dd2b6e588d30870eab0
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-04-06 08:38:07 -0700

    created about section

---
+++ src/components/About/About.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {useTranslation} from "react-i18next";
+import "./About.css";
+
+interface AboutSectionProps {
+    id: string;
+}
+
+const AboutSection: React.FC<AboutSectionProps> = ({id}) => {
+
+    const {t} = useTranslation();
+    return (
+        <div id={id} className="about-section section">
+            <h1>{t('AboutSection.Title')}</h1>
+            <p>{t('AboutSection.Paragraph1')}</p>
+            <p>{t('AboutSection.Paragraph2')}</p>
+            <p>{t('AboutSection.Paragraph3')}</p>
+            <p>{t('AboutSection.Paragraph4')}</p>
+            <p>{t('AboutSection.Paragraph5')}</p>
+        </div>
+    );
+};
+
+export default AboutSection;
