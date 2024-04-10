4461f9755df417d03551dc6a288ef1592b42f9d9
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-04-09 21:42:56 -0700

    minor style fixes

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -27,7 +27,7 @@ const ContactsSection: React.FC<ContactsSectionProps> = ({id}) => {
                         <img src={img} alt={"ecofuelplus logo"}/>
                     </div>
                 </div>
-                <div className="contact-card map-container">
+                <div className="map-container contact-card ">
                     <iframe
                         title="location"
                         // src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d5347.511876521549!2d25.141368402991134!3d50.762789293808886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDQ1JzQ5LjkiTiAyNcKwMDgnNTcuNiJF!5e0!3m2!1sen!2sca!4v1712360549709!5m2!1sen!2sca"
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,7 +9,7 @@ interface HomeSectionProps {
 const HomeSection: React.FC<HomeSectionProps> = ({id}) => {
     const {t} = useTranslation();
     return (
-        <div id={id} className="home-section section">
+        <div id={id} className="home-section">
             <h1>{t('Welcome')}</h1>
         </div>
     );
