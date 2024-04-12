624cedf8db524a058de01a930d18eca0f9ec9161
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-04-11 21:59:22 -0700

    changed table data

--- src/components/FuelComparison/FuelComparison.tsx
+++ src/components/FuelComparison/FuelComparison.tsx
@@ -28,7 +28,7 @@ const FuelComparisonSection: React.FC<FuelComparisonSectionProps> = ({id}) => {
                     <tr>
                         <td>{t("pellets.moisture")}</td>
                         <td>{"30-40"}</td>
-                        <td>{"7-10"}</td>
+                        <td>{"6-10"}</td>
                         <td>{"10-15"}</td>
                         <td>{"-"}</td>
                     </tr>
@@ -42,14 +42,14 @@ const FuelComparisonSection: React.FC<FuelComparisonSectionProps> = ({id}) => {
                     <tr>
                         <td>{t("pellets.calorificMjKg")}</td>
                         <td>{"9-12"}</td>
-                        <td>{"15-16"}</td>
+                        <td>{"15-19"}</td>
                         <td>{"23-28"}</td>
                         <td>{"35-48"}</td>
                     </tr>
                     <tr>
                         <td>{t("pellets.calorificKWH")}</td>
                         <td>{"4.2-4.5"}</td>
-                        <td>{"4.7-5.4"}</td>
+                        <td>{"4.7-5.5"}</td>
                         <td>{"6.4-7.5"}</td>
                         <td>{"9.6-13.3"}</td>
                     </tr>
