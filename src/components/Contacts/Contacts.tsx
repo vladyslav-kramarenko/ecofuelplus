0bc9b9ee1ae599bca238892af1aa61d5cbb4cab5
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-04-11 23:01:34 -0700

    updated google map link

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -30,7 +30,7 @@ const ContactsSection: React.FC<ContactsSectionProps> = ({id}) => {
                     <iframe
                         title="location"
                         // src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d5347.511876521549!2d25.141368402991134!3d50.762789293808886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDQ1JzQ5LjkiTiAyNcKwMDgnNTcuNiJF!5e0!3m2!1sen!2sca!4v1712360549709!5m2!1sen!2sca"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20179.71393935068!2d25.1234504!3d50.78550445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4725a365bd4d593d%3A0xfce34940def75dca!2sShepel&#39;%2C%20Volyn%20Oblast%2C%20Ukraine%2C%2045620!5e0!3m2!1sen!2sca!4v1711952372563!5m2!1sen!2sca"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28545.94485978734!2d25.111469732822012!3d50.77318408832637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4725a36936b16ca3%3A0xfbe50dc941b095e9!2z0JXQmtCe0KTQrNCu0JXQmyDQn9Cb0K7QoQ!5e0!3m2!1sen!2sca!4v1712900768615!5m2!1sen!2sca"
                         width="600"
                         height="450"
                         style={{border: 0}}
