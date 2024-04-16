e54d919f721938d15d7315602d55dcbd91759bbc
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-04-15 22:56:47 -0700

    added animation

--- src/components/Certificats/Certificates.tsx
+++ src/components/Certificats/Certificates.tsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import "./Certificates.css";
-import Modal from "./Modal/Modal";
+import Modal from "../Modal/Modal";
 import certificate1 from "../../images/certificate.jpg"
 import certificate2 from "../../images/certificate_ua.jpg"
 import {useTranslation} from "react-i18next";
-
+import { motion } from 'framer-motion';
+import {useInView} from "react-intersection-observer";
 interface CertificatsSectionProps {
     id: string;
 }
@@ -25,13 +26,27 @@ const CertificatesSection: React.FC<CertificatsSectionProps> = ({id}) => {
         setModalOpen(true);
     };
 
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.3,
+    });
+
+    const variants = {
+        visible: { opacity: 1, x: 0, y:0, transition: { type: 'spring', duration: 1 } },
+        hidden: { opacity: 0, x: 100, y:0 },
+    };
+
     return (
         <section id={id} className="certificates-section section">
             <h2>{t("certificates.title")}</h2>
             <p>{t("certificates.description")}</p>
             <div className="gallery">
                 {certificates.map(certificate => (
-                    <img
+                    <motion.img
+                        ref={ref}
+                        initial="hidden"
+                        animate={inView ? 'visible' : 'hidden'}
+                        variants={variants}
                         key={certificate.id}
                         src={certificate.src}
                         alt={certificate.alt}
