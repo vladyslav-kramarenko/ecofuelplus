0006f0d2915cef5a5ca1a4acc4964fc411b954bb
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-04-16 15:38:56 -0700

    added logo to home section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useTranslation} from "react-i18next";
 import "./Home.css";
+import logo from '../../images/icon_big-min.png'
 
 interface HomeSectionProps {
     id: string;
@@ -10,7 +11,8 @@ const HomeSection: React.FC<HomeSectionProps> = ({id}) => {
     const {t} = useTranslation();
     return (
         <div id={id} className="home-section">
-            <h1>{t('Welcome')}</h1>
+            {/*<h1>{t('Welcome')}</h1>*/}
+            <img src={logo} className={"home-logo"} alt={"Ecofuel Logo"}/>
         </div>
     );
 };
